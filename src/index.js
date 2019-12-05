95d386990610ecd27f9215942d0daedfa2200f9b
Nerissa Taylor <taylornerissa@hotmail.com>
2019-12-05 15:55:43 -0500

    fix link color

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const App = () => {
         <nav>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              {/* <Link to="/">Home</Link> */}
 
             </li>
             <li><Link to="/navbar">Nav</Link></li>
